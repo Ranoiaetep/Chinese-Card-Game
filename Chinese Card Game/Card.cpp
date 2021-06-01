f97a650b11b6f3d1f7e0d2854f5e18f0ecd0d823
Ranoiaetep <ranoiaetep@me.com>
2021-06-01 12:57:57 -0700

    Initial Commit

---
+++ Chinese Card Game/Card.cpp
@@ -0,0 +1,8 @@
+//
+//  Card.cpp
+//  Chinese Card Game
+//
+//  Created by Ranoiaetep on 6/1/21.
+//
+
+#include "Card.hpp"
---
+++ Chinese Card Game/Card.hpp
@@ -0,0 +1,13 @@
+//
+//  Card.hpp
+//  Chinese Card Game
+//
+//  Created by Ranoiaetep on 6/1/21.
+//
+
+#ifndef Card_hpp
+#define Card_hpp
+
+#include <stdio.h>
+
+#endif /* Card_hpp */
